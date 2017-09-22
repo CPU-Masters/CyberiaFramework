19f1ebf298657bb8fde2186f2aae2e5b343ff410
Josh Benton <josh@styleconnect.net>
2017-09-22 09:27:36 -0400

    database field progress

--- src/main/java/Cyberia/CyberiaFramework/database/storage/DatabaseField.java
+++ src/main/java/Cyberia/CyberiaFramework/database/storage/DatabaseField.java
@@ -1,5 +1,14 @@
 package Cyberia.CyberiaFramework.database.storage;
-
-public class DatabaseField {
-
+/**
+ * Class that holds a field for a database object.
+ * @author Inhaler
+ *
+ * @param <T> The Java type
+ * @param <S> The MySQL type
+ */
+public class DatabaseField<T,S> {
+	
+	public DatabaseField(String dbName,T defaultValue,DatabaseObject dbObject) {
+		
+	}
 }
